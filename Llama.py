8ab6217b516f5c5e504127bd1675d4636a23fe2c
Daniel Lewis <daniel.lewis.ab@gmail.com>
2024-01-06 13:22:38 -0700

    Initial commit

---
+++ Llama.py
@@ -0,0 +1,100 @@
+
+import sys
+import os
+import folder_paths
+
+# Folder path and venv
+llama_dir = os.path.dirname(os.path.realpath(__file__))
+venv_site_packages = os.path.join(folder_paths.base_path, 'venv', 'Lib', 'site-packages')
+sys.path.append(venv_site_packages)
+
+# Attempt to get llama_cpp if it doesn't exist
+try:
+    from llama_cpp import Llama
+except ImportError:
+
+    # Determine the correct path based on the operating system
+    if os.name == 'posix':
+        site_packages = os.path.join(sys.prefix, 'lib', 'python{}.{}/site-packages'.format(sys.version_info.major, sys.version_info.minor))
+    else:  # For Windows
+        site_packages = os.path.join(sys.prefix, 'Lib', 'site-packages')
+
+    sys.path.append(site_packages)
+    from llama_cpp import Llama
+
+# Inject 'llm' to folder_paths ourselves, so we can use it like we belong there and have behavioral consistency
+supported_file_extensions = set(['.gguf'])
+models_dir = os.path.join(llama_dir, "models")
+folder_paths.folder_names_and_paths["llm"] = ([models_dir], supported_file_extensions)
+
+class LLM_Load_Model:
+
+    # A hopefully thin wrapper for the Llama class to bind it to ComfyUI
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "Model": (folder_paths.get_filename_list("llm"), ), 
+            },
+            "optional": {
+                "n_ctx": ("INT", {"default": 0, "step":512, "min":0}),
+            }
+        }
+
+    # ComfyUI will effectively return the Llama class instanciation provided by execute() and call it an LLM
+    RETURN_TYPES = ("LLM",)
+    FUNCTION = "execute"
+    CATEGORY = "LLM"
+
+    def execute(self, Model, n_ctx):
+
+        # basically just calls __init__ on the Llama class
+        model_path = folder_paths.get_full_path("llm", Model)
+        llm = Llama(model_path=model_path, chat_format="llama-2", n_ctx=n_ctx, seed=-1)
+
+        return (llm,)
+
+class LLM_Generate:
+
+    # A hopefully thin wrapper for the Llama generate method to bind it to ComfyUI
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "LLM":("LLM",),
+                "prompt":("STRING", {"default":"", "multiline":True}),
+            },
+            "optional": {
+                "max_response_tokens": ("INT", {"default": 0}),
+                "temperature": ("FLOAT", {"default": 0.8, "min":0.0, "max":1.0, "step":0.01, "round":0.01, "display":"number"}),
+                "seed": ("INT", {"default": -1}),
+            }
+        }
+
+    # ComfyUI will effectively return the text result of the Llama.__call__() as a STRING
+    RETURN_TYPES = ("STRING",)
+    FUNCTION = "execute"
+    CATEGORY = "LLM"
+
+    def execute(self, LLM, prompt, max_response_tokens, temperature, seed):
+
+        # I'm using __call__ and not generate() because seed isn't available in generate!
+        response = LLM.__call__(
+            prompt=prompt, 
+            max_tokens=max_response_tokens, 
+            temperature=temperature, 
+            seed=seed,
+            stop=LLM.token_eos() )
+        # print(response['choices'][0]['text'])
+        return (response['choices'][0]['text'], )
+
+NODE_CLASS_MAPPINGS = {
+    "LLM_Load_Model":LLM_Load_Model,
+    "LLM_Generate":LLM_Generate,
+}
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "LLM_Load_Model": "LLM_Load_Model",
+    "LLM_Generate":"LLM_Generate",
+}
+
+
--- README.md
+++ README.md
@@ -1,4 +1,43 @@
-# ComfyUI-Llama
-Bridging wrapper for llama-cpp-python within ComfyUI
+# Llama for ComfyUI
+Improved [llama-cpp](https://github.com/abetlen/llama-cpp-python) integration for ComfyUI.
 
-[I apologize for the delay, it's now 1am, I will commit tomorrow]
+# Installation
+
+## If using Comfy Manager:
+Not available at this time.
+
+## If installing manually:
+1. Clone this repo into `custom_nodes` folder.
+2. Install llama-cpp-python.
+
+# How to Use:
+1. Download GGUF language learning models, which can be found on HuggingFace. You will need at least 1. Different models produce different results.
+2. Place models in ```ComfyUI/custom_nodes/ComfyUI-Llama/models```. They can be renamed if you want.
+3. Fire up/Restart ComfyUI and allow it to finish restarting.
+4. Hit Ctrl+F5 to ensure the browser is refreshed.
+5. Check your ComfyUI available nodes and find the menu for ours.
+
+# Notable Updates
+Current version is 0.1.0
+
+# Features:
+Currently uses dropdowns to load GGUF models in a consistent fashion with other ComfyUI nodes and can use them to generate strings of output text with seemingly correct seeding and temperature.
+
+# Upcoming features:
+
+# Core Nodes:
+
+# Usage
+See the example1.json or example1.png file.
+See (https://abetlen.github.io/llama-cpp-python/) for documentation on the llama-cpp-python interface
+
+# Known Issues
+1. No known effective way to output long strings of text to nodes within ComfyUI for examination by users.
+2. No known way to loopback output from an LLM model repeatedly, but I'm investigating Loopchain.
+3. Haven't widely tested models, operating systems, file path trickery, emulators, or strange abuses of inputs.
+4. Will simply crash if llama-cpp-python throws an Error.
+5. Haven't thoroughly tested installer.
+6. Haven't tried to list the custom_node with Comfy Manager yet.
+
+
+
